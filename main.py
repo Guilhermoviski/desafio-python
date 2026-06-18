5a1b13684632c8f988e6e259fd3b7672f5a69b5f
Guilhermoviski <guilhermebrito2106@gmail.com>
2026-06-18 16:49:07 -0300

    criando loop para poder mandar a mensagem para cada pessoa da tabela.

--- main.py
+++ main.py
@@ -13,3 +13,27 @@ ID_TOKEN = os.getenv("ZAPI_TOKEN")
 ID_CLIENTE_TOKEN = os.getenv("ZAPI_CLIENTE_TOKEN")
 
 url_zapi = f"https://api.z-api.io/instances/{ID_INSTANCIA}/token/{ID_TOKEN}/send-text"
+
+headers = {
+    "Client-Token": ID_CLIENTE_TOKEN,
+    "Content-Type": "application/json"
+}
+
+supabase = create_client(URL, KEY)
+
+dados = (supabase.table("pessoas").select("*").execute())
+
+for pessoa in dados.data:
+    mensagem = {
+        "phone": pessoa["telefone"],
+        "message": f"Olá, {pessoa['nome_pessoa']} tudo bem com você?"
+    }
+
+    response = requests.post(
+    url_zapi,
+    headers=headers,
+    json=mensagem
+    )
+
+    print(response.status_code)
+    print(response.text)
